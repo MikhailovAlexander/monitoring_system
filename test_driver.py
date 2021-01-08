8adc81d28b20bd166a3c3157b7bd3bb98dfd7a82
MikhailovAlexander <vestroy@gmail.com>
2021-01-08 20:05:50 +0500

    #test_db test_driver.py

---
+++ test_driver.py
@@ -0,0 +1,13 @@
+from sqlite_db_driver import SqliteDbDriver
+
+
+def test(driver):
+    if not driver.is_db_exist():
+        driver.init_db()
+        print('db created')
+    else:
+        print('db already exist')
+
+
+driver = SqliteDbDriver('./db/test_db', './db/init_db.sql')
+test(driver)
