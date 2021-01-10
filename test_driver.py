7380cfe0bf61d2989175f1cf2f80a36a4ab2c084
MikhailovAlexander <vestroy@gmail.com>
2021-01-10 23:20:47 +0500

    #test_db test for user table methods added

--- test_driver.py
+++ test_driver.py
@@ -1,4 +1,5 @@
 from sqlite_db_driver import SqliteDbDriver
+from datetime import datetime
 
 
 def test(driver):
@@ -8,8 +9,31 @@ def test(driver):
     else:
         print('db already exist')
 
+def test_user(driver):
+    print('test_user')
+    if not driver.chk_conn():
+        driver.get_connection()
+    print(driver.user_rda())
+    name = 'test' + str(datetime.now())
+    print(f'name: {name}')
+
+    print('ins')
+    driver.user_ins(name)
+    res = driver.user_rda()
+    print(res)
+    id = int(res[-1][0])
+    print(id)
+
+    print('upd')
+    driver.user_upd(id, name + '_upd')
+    print(driver.user_rd(id))
+
+    print('dlt')
+    driver.user_dlt(id)
+    print(driver.user_rda())
 
 driver = SqliteDbDriver('./db/test_db.db', './db/init_db.sql')
 test(driver)
+test_user(driver)
 driver.close_connection()
 
