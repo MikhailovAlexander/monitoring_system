e27e67cbdd47fb228534bdfe4a278582af063b7f
MikhailovAlexander <vestroy@gmail.com>
2021-01-15 00:58:01 +0500

    #dev init_db added

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import tkinter as tk
-from form.EntryForm import EntryForm
+from forms.EntryForm import EntryForm
 from sqlite_db_driver import SqliteDbDriver
 from core.User import User
 
 root = tk.Tk()
 driver = SqliteDbDriver('./db/test_db.db', './db/init_db.sql')
+if not driver.is_db_exist():
+    driver.init_db()
 driver.get_connection()
 user = User(driver)
 app = EntryForm(root, driver, user)
